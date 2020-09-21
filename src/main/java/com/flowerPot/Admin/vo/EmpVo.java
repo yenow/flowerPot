1061297015a65be157c4d1bcdeb64b775e99ea09
YooHyeok <yjk7454@naver.com>
2020-09-21 20:45:40 +0900

    20200921

---
+++ src/main/java/com/flowerPot/Admin/vo/EmpVo.java
@@ -0,0 +1,17 @@
+package com.flowerPot.Admin.vo;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class EmpVo {
+	
+	private Integer empNo;
+	private String empName;
+	private String empId;
+	private String deptNo;
+	private String poNo;
+}
