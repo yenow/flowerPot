e91582a12e42a3dd67461b4d489c77fcf12aa871
ysy <phantom_ysy@naver.com>
2020-10-19 22:51:08 +0900

    start brandAdmin work
    start brandAdmin work

---
+++ src/main/java/com/flowerPot/brandAdmin/controller/BrandAdminController.java
@@ -0,0 +1,15 @@
+package com.flowerPot.brandAdmin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("brandAdmin")
+public class BrandAdminController {
+
+	@RequestMapping("dashboard")
+	public void dashboard() {
+		
+	}
+}
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,4 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
+<c:set var="path" value="${pageContext.request.contextPath }" scope="application"></c:set>
 
 <jsp:include page="info/header3.jsp"></jsp:include>
 
