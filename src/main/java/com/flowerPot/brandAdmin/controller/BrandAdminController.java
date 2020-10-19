cd84223241e9933991cc64a44af570c3a880804c
ysy <phantom_ysy@naver.com>
2020-10-20 00:17:09 +0900

    change admin UI templete

--- src/main/java/com/flowerPot/brandAdmin/controller/BrandAdminController.java
+++ src/main/java/com/flowerPot/brandAdmin/controller/BrandAdminController.java
@@ -12,4 +12,10 @@ public class BrandAdminController {
 	public void dashboard() {
 		
 	}
+	
+	
+	@RequestMapping("test")
+	public void test() {
+		
+	}
 }
--- src/main/webapp/resources/brandAdmin/dist/js/sidebarmenu.js
+++ src/main/webapp/resources/brandAdmin/dist/js/sidebarmenu.js
@@ -11,6 +11,7 @@ $(function() {
     "use strict";
      var url = window.location + "";
         var path = url.replace(window.location.protocol + "//" + window.location.host + "/", "");
+        console.log(path);
         var element = $('ul#sidebarnav a').filter(function() {
             return this.href === url || this.href === path;// || url.href.indexOf(this.href) === 0;
         });
